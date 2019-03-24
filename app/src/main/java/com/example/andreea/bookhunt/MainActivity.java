2ec2ee7778eed42c98676f36c19bb83f6a68c12b
ursuadina <ursuadina98@yahoo.com>
2019-03-24 16:38:20 +0200

    first page without functionality

--- app/src/main/java/com/example/andreea/bookhunt/MainActivity.java
+++ app/src/main/java/com/example/andreea/bookhunt/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.andreea.bookhunt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //TODO: log in
+    public void btnLogInOnClick(View view) {
+        Toast.makeText(MainActivity.this, "TO DO: Log in on click", Toast.LENGTH_SHORT)
+                .show();
+    }
+    //TODO: sign in
+    public void btnSignInOnClick(View view) {
+        Toast.makeText(MainActivity.this, "TO DO: Sign in on click", Toast.LENGTH_SHORT)
+                        .show();
+    }
+    //TODO: log in with google
+    public void btnSignInGoogleOnClick(View view) {
+        Toast.makeText(MainActivity.this, "TO DO: Log in with Google on click",
+                Toast.LENGTH_SHORT).show();
     }
 }
