9a49d081bc8d6e4cb3cb0ad67fbe7601fcfd80ec
ursuadina <ursuadina98@yahoo.com>
2019-04-12 13:51:54 +0300

    add nav drawer

--- app/src/main/java/com/example/andreea/bookhunt/LogInActivity.java
+++ app/src/main/java/com/example/andreea/bookhunt/LogInActivity.java
@@ -1,7 +1,6 @@
 package com.example.andreea.bookhunt;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,16 +14,12 @@ import android.widget.Toast;
 import com.example.andreea.bookhunt.models.Register;
 import com.example.andreea.bookhunt.utils.Constants;
 import com.example.andreea.bookhunt.utils.SharedPreferencesHelper;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
