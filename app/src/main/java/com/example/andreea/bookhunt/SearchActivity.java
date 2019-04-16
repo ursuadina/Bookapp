05b03adcb50dcb037fe42dbfbd6b01be4a340333
ursuadina <ursuadina98@yahoo.com>
2019-04-16 14:46:02 +0300

    attempt to get text from photo

--- app/src/main/java/com/example/andreea/bookhunt/SearchActivity.java
+++ app/src/main/java/com/example/andreea/bookhunt/SearchActivity.java
@@ -3,17 +3,22 @@ package com.example.andreea.bookhunt;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.andreea.bookhunt.utils.Constants;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 import org.w3c.dom.Text;
 
@@ -44,6 +49,12 @@ public class SearchActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
         imageViewBook.setImageBitmap(bitmap);
 
+        String text = imageToText();
+        String text1 = textViewBookName.getText().toString();
+
+        String textNou = text1 + text;
+        textViewBookName.setText(textNou);
+
     }
 
     public void initView() {
@@ -67,6 +78,31 @@ public class SearchActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imageViewBook.setImageBitmap(imageBitmap);
+
+            String text = imageToText();
+            String text1 = textViewBookName.getText().toString();
+
+            String textNou = text1 + text;
+            textViewBookName.setText(textNou);
         }
+    }
+
+    public String imageToText() {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        Bitmap bitmap = ((BitmapDrawable)imageViewBook.getDrawable()).getBitmap();
+        Frame imageFrame = new Frame.Builder()
+                .setBitmap(bitmap)                 // your image bitmap
+                .build();
+
+        StringBuilder imageText = new StringBuilder();
+
+
+        SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
+
+        for (int i = 0; i < textBlocks.size(); i++) {
+            TextBlock textBlock = textBlocks.valueAt(i);
+            imageText.append(textBlock.getValue());
+        }
+        return imageText.toString();
     }
 }
